29107dd7b2c82e3a87dd7b8e9b060023b58440fc
Mark Anthony <markanthonymartinez9@gmail.com>
2018-08-13 03:55:10 -0500

    Setup eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 const gulp = require('gulp');
+const eslint = require('eslint');
 
-gulp.task('default', [], function() {
+gulp.task('default', ['lint'], function() {
+	gulp.watch('js/**/*.js', ['lint']);
+});
 
+gulp.task('lint', function() {
+	return gulp.src(['js/**/*.js'])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failOnError());
 });
