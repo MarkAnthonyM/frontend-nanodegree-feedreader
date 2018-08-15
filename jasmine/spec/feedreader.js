1ec9b139296a45147a6779b0aeada985d5b0b2cb
Mark Anthony <markanthonymartinez9@gmail.com>
2018-08-14 22:54:51 -0500

    Finish rss feed suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,24 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('has url', function() {
+           allFeeds.forEach(function(element) {
+             expect(element.url).toBeDefined();
+             expect(element.url.length).not.toBe(0);
+           })
+         });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('has name', function() {
+           allFeeds.forEach(function(element) {
+             expect(element.name).toBeDefined();
+             expect(element.name.length).not.toBe(0);
+           })
+         });
     });
 
 
